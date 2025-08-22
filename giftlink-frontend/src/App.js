11bad70ab02a9286f5f5f6d0a194d0746a39df94
jonesdev728 <sentexas.jj@gmail.com>
2025-08-22 10:37:27 -0500

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   return (
         <>
         <Navbar/>
